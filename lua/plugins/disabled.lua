811d9fdc2ce965b9297ce1b0f282656eb6636e5a
Mathieu Rossi <mathieu-rossi.pro@proton.me>
2024-03-25 08:37:59 +0100

    init

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,8 @@
+return {
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "nvim-pack/nvim-spectre", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "nvim-lualine/lualine.nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  { "folke/persistence.nvim", enabled = false },
+}

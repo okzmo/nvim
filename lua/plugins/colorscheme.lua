de7a501c7e75fbf2e6cf0d91b094b9d354441ceb
Mathieu Rossi <mathieu-rossi.pro@proton.me>
2024-04-17 08:38:26 +0200

    feat: colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  { "Domeee/mosel.nvim" },
+  { "mcchrish/zenbones.nvim", dependencies = { "rktjmp/lush.nvim" } },
+  { "rebelot/kanagawa.nvim" },
+  { "ramojus/mellifluous.nvim" },
+  { "mellow-theme/mellow.nvim" },
+  { "Yazeed1s/oh-lucy.nvim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "mellow",
+    },
+  },
+}
--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,6 +3,5 @@ return {
   { "nvim-pack/nvim-spectre", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-lualine/lualine.nvim", enabled = false },
-  { "folke/noice.nvim", enabled = false },
   { "folke/persistence.nvim", enabled = false },
 }
